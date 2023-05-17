4edb0b9383c174be7ddb6f5f533c4cb8b04e769b
Documenter.jl <documenter@juliadocs.github.io>
2023-05-17 15:00:12 +0000

    build based on f63f2ed

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CIndices","category":"page"},{"location":"#CIndices","page":"Home","title":"CIndices","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CIndices.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CIndices]","category":"page"}]
+}
