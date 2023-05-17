218f8fefd8d20123febac7ddaa9943056b16ab83
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-05-17 09:29:42 -0400

    Copy more stuff from Finch.jl

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
 # CIndices
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://Wimmerer.github.io/CIndices.jl/stable/)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://Wimmerer.github.io/CIndices.jl/dev/)
-[![Build Status](https://github.com/Wimmerer/CIndices.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/Wimmerer/CIndices.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://JuliaSparse.github.io/CIndices.jl/stable/)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://JuliaSparse.github.io/CIndices.jl/dev/)
+[![Build Status](https://github.com/JuliaSparse/CIndices.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/JuliaSparse/CIndices.jl/actions/workflows/CI.yml?query=branch%3Amain)
 
-Incrementer?! I hardly know'er!
+Julia, Matlab, etc. index arrays [starting at
+1](https://docs.julialang.org/en/v1/devdocs/offset-arrays/). C, python, etc.
+index starting at 0. In a dense array, we can simply subtract one from the
+index, and in fact, this is what Julia will does under the hood when you pass a
+vector [between C to
+Julia](https://docs.julialang.org/en/v1/manual/embedding/#Working-with-Arrays). 
+
+However, for sparse array formats, it's not just a matter of subtracting one
+from the index, as the internal lists of indices, positions, etc all start from
+zero as well. To remedy the situation, this package exports a handy zero-indexed integer
+type called `CIndex`. The internal representation of `CIndex` is one less than the
+value it represents, and we can use `CIndex` as the index or position type of
+a sparse array to represent arrays in other languages.
+
+For example, if `idx_c`, `ptr_c`, and `val` are the internal arrays of a CSC
+matrix in a zero-indexed language, we can represent that matrix as a one-indexed
+Cindex array without copying by calling
+```julia
+julia> m = 4; n = 3; ptr_c = [0, 3, 3, 5]; idx_c = [1, 2, 3, 0, 2]; val_c = [1.1, 2.2, 3.3, 4.4, 5.5];
+
+
+julia> m = 4; n = 3; ptr_c = [0, 3, 3, 5]; idx_c = [1, 2, 3, 0, 2]; val = [1.1, 2.2, 3.3, 4.4, 5.5];
+
+julia> ptr_jl = unsafe_wrap(Array, reinterpret(Ptr{CIndex{Int}}, pointer(ptr_c)), length(ptr_c); own = false)
+4-element Vector{CIndex{Int64}}:
+ CIndex{Int64}(0)
+ CIndex{Int64}(3)
+ CIndex{Int64}(3)
+ CIndex{Int64}(5)
+
+julia> idx_jl = unsafe_wrap(Array, reinterpret(Ptr{CIndex{Int}}, pointer(idx_c)), length(idx_c); own = false)
+5-element Vector{CIndex{Int64}}:
+ CIndex{Int64}(1)
+ CIndex{Int64}(2)
+ CIndex{Int64}(3)
+ CIndex{Int64}(0)
+ CIndex{Int64}(2)
+
+julia> A = SparseMatrixCSC{Float64, CIndex{Int}}(m, n, ptr_jl, idx_jl, val)
+4×3 SparseMatrixCSC{Float64, CIndex{Int64}} with 5 stored entries:
+  ⋅    ⋅   4.4
+ 1.1   ⋅    ⋅ 
+ 2.2   ⋅   5.5
+ 3.3   ⋅    ⋅ 
+```
--- src/CIndices.jl
+++ src/CIndices.jl
@@ -1,47 +1,49 @@
 module CIndices
 
+export CIndex
+
 # Courtesy of the fantastique Willow Ahrens
-struct Cindex{T} <: Integer
+struct CIndex{T} <: Integer
     val::T
-    Cindex{T}(i, b::Bool=true) where {T} = new{T}(T(i) - b)
-    Cindex{T}(i::Cindex{T}) where {T} = i
+    CIndex{T}(i, b::Bool=true) where {T} = new{T}(T(i) - b)
+    CIndex{T}(i::CIndex{T}) where {T} = i
 end
 
 cindex_types = [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128, BigInt]
 for S in cindex_types
     @eval begin
-        @inline Base.promote_rule(::Type{Cindex{T}}, ::Type{$S}) where {T} = promote_type(T, $S)
-        Base.convert(::Type{Cindex{T}}, i::$S) where {T} = Cindex(convert(T, i))
-        Cindex(i::$S) = Cindex{$S}(i)
-        (::Type{$S})(i::Cindex{T}) where {T} = convert($S, i.val + true)
-        Base.convert(::Type{$S}, i::Cindex) = convert($S, i.val + true)
-        @inline Base.:(<<)(a::Cindex{T}, b::$S) where {T} = T(a) << b
+        @inline Base.promote_rule(::Type{CIndex{T}}, ::Type{$S}) where {T} = promote_type(T, $S)
+        Base.convert(::Type{CIndex{T}}, i::$S) where {T} = CIndex(convert(T, i))
+        CIndex(i::$S) = CIndex{$S}(i)
+        (::Type{$S})(i::CIndex{T}) where {T} = convert($S, i.val + true)
+        Base.convert(::Type{$S}, i::CIndex) = convert($S, i.val + true)
+        @inline Base.:(<<)(a::CIndex{T}, b::$S) where {T} = T(a) << b
     end
 end
 for S in [Float32, Float64]
     @eval begin
-        @inline Base.promote_rule(::Type{Cindex{T}}, ::Type{$S}) where {T} = Cindex{promote_type(T, $S)}
-        (::Type{$S})(i::Cindex{T}) where {T} = convert($S, i.val + true)
+        @inline Base.promote_rule(::Type{CIndex{T}}, ::Type{$S}) where {T} = CIndex{promote_type(T, $S)}
+        (::Type{$S})(i::CIndex{T}) where {T} = convert($S, i.val + true)
     end
 end
-Base.promote_rule(::Type{Cindex{T}}, ::Type{Cindex{S}}) where {T, S} = promote_type(T, S)
-Base.convert(::Type{Cindex{T}}, i::Cindex) where {T} = Cindex{T}(convert(T, i.val), false)
-Base.hash(x::Cindex, h::UInt) = hash(typeof(x), hash(x.val, h))
+Base.promote_rule(::Type{CIndex{T}}, ::Type{CIndex{S}}) where {T, S} = promote_type(T, S)
+Base.convert(::Type{CIndex{T}}, i::CIndex) where {T} = CIndex{T}(convert(T, i.val), false)
+Base.hash(x::CIndex, h::UInt) = hash(typeof(x), hash(x.val, h))
 
 for op in [:*, :+, :-, :min, :max]
-    @eval @inline Base.$op(a::Cindex{T}, b::Cindex{T}) where {T} = Cindex($op(T(a), T(b)))
+    @eval @inline Base.$op(a::CIndex{T}, b::CIndex{T}) where {T} = CIndex($op(T(a), T(b)))
 end
 
 for op in [:*, :+, :-, :min, :max]
-    @eval @inline Base.$op(a::Cindex{T}) where {T} = Cindex($op(T(a)))
+    @eval @inline Base.$op(a::CIndex{T}) where {T} = CIndex($op(T(a)))
 end
 
 for op in [:<, :<=, :isless]
-    @eval @inline Base.$op(a::Cindex{T}, b::Cindex{T}) where {T} = $op(T(a), T(b))
+    @eval @inline Base.$op(a::CIndex{T}, b::CIndex{T}) where {T} = $op(T(a), T(b))
 end
 
 for op in [:typemin, :typemax]
-    @eval @inline Base.$op(::Type{Cindex{T}}) where {T} = Base.$op(T)
+    @eval @inline Base.$op(::Type{CIndex{T}}) where {T} = Base.$op(T)
 end
 
 end
